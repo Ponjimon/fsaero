aefd3d0dfed66fe8421b7026d9358a70e45cef87
Simon <sn@hitbox.tv>
2021-09-01 23:16:02 +0200

    chore: remove nx demo page

--- apps/ui/pages/_app.tsx
+++ apps/ui/pages/_app.tsx
@@ -1,24 +1,11 @@
 import { AppProps } from 'next/app';
-import Head from 'next/head';
-import './styles.css';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head>
-        <title>Welcome to ui!</title>
-      </Head>
-      <div className="app">
-        <header className="flex">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src="/nx-logo-white.svg" alt="Nx logo" width="75" height="50" />
-          <h1>Welcome to ui!</h1>
-        </header>
-        <main>
-          <Component {...pageProps} />
-        </main>
-      </div>
-    </>
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
   );
 }
 
