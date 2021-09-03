eb3c552986283d181103b94a98a3ef615f2974a0
Simon <sn@hitbox.tv>
2021-09-04 00:21:57 +0200

    chore: test gcp

--- apps/ui/src/utils/encrypt.ts
+++ apps/ui/src/utils/encrypt.ts
@@ -1,6 +1,9 @@
 import { createCipheriv } from 'crypto';
 
 export const encrypt = (message: string) => {
+  if (!process.env.ENCRYPTION_KEY || !process.env.ENCRYPTION_IV) {
+    throw new Error('ENCRYPTION_KEY or ENCRYPTION_IV is not defined');
+  }
   const cipher = createCipheriv(
     'aes-128-cbc',
     process.env.ENCRYPTION_KEY,
