14f63d869fe964aa93dcd5612c3d4c18e269b702
Simon <sn@hitbox.tv>
2021-09-05 17:58:03 +0200

    refactor: remove unused files

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
 import { FSAirlinesGQLModule } from '@fsaero/fsairlines-gql';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [FSAirlinesGQLModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
