2adb84c33cb120008e553f5b358d7c498742dca9
Simon <sn@hitbox.tv>
2021-09-10 16:56:55 +0200

    chore: fix ignore vercel build script

--- apps/ui/ignore-vercel-build.sh
+++ apps/ui/ignore-vercel-build.sh
@@ -1,11 +1,11 @@
-# Name of the app to check. Change this to your application name!
+#!/bin/bash
 APP=ui
 # Determine version of Nx installed
 NX_VERSION=$(node -e "console.log(require('./package.json').devDependencies['@nrwl/workspace'])")
 
 # Install @nrwl/workspace in order to run the affected command
 yarn --version
-yarn add --dev @nrwl/workspace@$NX_VERSION --cached
+yarn add --dev @nrwl/workspace@"${NX_VERSION}" --cached
 
 # Run the affected command, comparing latest commit to the one before that
 npx nx affected:apps --plain --base HEAD~1 --head HEAD | grep $APP -q
@@ -13,10 +13,10 @@ npx nx affected:apps --plain --base HEAD~1 --head HEAD | grep $APP -q
 # Store result of the previous command (grep)
 IS_AFFECTED=$?
 
-if [ $IS_AFFECTED -eq 1 ]; then
+if [ "${IS_AFFECTED}" -eq 1 ] || [[ "${VERCEL_GIT_COMMIT_REF}" =~ /^renovate/ ]]; then
   echo "🛑 - Build cancelled"
   exit 0
-elif [ $IS_AFFECTED -eq 0 ]; then
+elif [ "${IS_AFFECTED}" -eq 0 ]; then
   echo "✅ - Build can proceed"
   exit 1
 fi
